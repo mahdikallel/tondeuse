1ecbd98ae4a1e627f78a039d742d52652a69c870
mahdi.kallel <mahdii.kallel@gmail.com>
2023-01-29 20:42:34 +0100

    conf: create project config Spring

---
+++ src/main/java/com/sg/tondeuse/TondeuseApplication.java
@@ -0,0 +1,14 @@
+package com.sg.tondeuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TondeuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TondeuseApplication.class, args);
+    }
+
+}
+
